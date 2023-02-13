0a09d9208296ec891250b14601aa001ec25ec700
Kathryn Neugent <kathrynneugent@gmail.com>
2023-02-13 16:03:13 -0700

    Add Docstrings

--- HMStoDD.py
+++ HMStoDD.py
@@ -1,8 +1,21 @@
 import numpy as np
 
-# inputs ra in hms and dec in dms
-# outputs array of [ra, dec] in decimal degrees
 def toDD (raH, raM, raS, decD, decM, decS):
+    '''
+    Returns a 2D array of RA and Dec in decimal degrees.
+   
+         Parameters:
+              raH (int): RA (hours)
+              raM (int): RA (minutes)
+              raS (int): RA (seconds)
+              decD (int): Declination (degrees)
+              decM (int): Declination (minutes)
+              decS (int): Declination (seconds)
+
+         Returns:
+              raDecDD ([int,int]): array of RA and Dec in decimal degrees
+    '''
     raDD = (raH)*15.+raM/4.+raS/240.
     decDD = (np.abs(decD)+decM/60.+decS/3600.)*np.sign(decD)
-    return [raDD,decDD]
+    raDecDD = [raDD,decDD]
+    return raDecDD
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # HMStoDD
-Convert a list of sexagesimal coordinates to a list of decimal degree coordinates.
-
-Note: Yes, this can be done in astropy, but it is needlessly slow.
+A small method used to convert a list of sexagesimal coordinates to a list of decimal degree coordinates.
 
