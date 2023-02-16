19f3cd63fd85268346ca57ba496a89539801ac18
Kathryn Neugent <kathrynneugent@gmail.com>
2023-02-15 20:37:29 -0700

    Update docstrings

--- HMStoDD.py
+++ HMStoDD.py
@@ -5,15 +5,15 @@ def toDD (raH, raM, raS, decD, decM, decS):
     Returns a 2D array of RA and Dec in decimal degrees.
    
          Parameters:
-              raH (int): RA (hours)
-              raM (int): RA (minutes)
-              raS (int): RA (seconds)
-              decD (int): Declination (degrees)
-              decM (int): Declination (minutes)
-              decS (int): Declination (seconds)
+              raH (float): RA (hours)
+              raM (float): RA (minutes)
+              raS (float): RA (seconds)
+              decD (float): Declination (degrees)
+              decM (float): Declination (minutes)
+              decS (float): Declination (seconds)
 
          Returns:
-              raDecDD ([int,int]): array of RA and Dec in decimal degrees
+              raDecDD ([float,float]): array of RA and Dec in decimal degrees
     '''
     raDD = (raH)*15.+raM/4.+raS/240.
     decDD = (np.abs(decD)+decM/60.+decS/3600.)*np.sign(decD)
